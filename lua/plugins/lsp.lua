1f03c2749f52f0eb0c54ed6e0dadbccdc4dcdcdc
louis <louis.pocheron@davikingcode.com>
2026-05-11 17:20:06 +0200

    chore: autoformat on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,13 @@ return {
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
             vim.keymap.set({ 'n' }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true }),
+                callback = function(args)
+                    vim.lsp.buf.format({ bufnr = args.buf, async = false, timeout_ms = 2000 })
+                end,
+            })
         end
     },
     {
